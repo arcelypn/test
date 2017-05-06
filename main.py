7f517c4dd723b727ab4228e03a7cd0a01d83d20a
Yatten <jonel.mawirat@bugomi.com>
2017-05-06 08:11:46 +0000

    hostname configuration

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-#fifth commit
+import pexpect
+child = pexpect.spawn("telnet 54.250.239.250 32773")
+child.sendline("\r\n")
+child.expect(">")
+child.sendline("enable")
+child.expect("#"
+child.sendline("config t")
+child.expect("#")
+child.sendline("hostanme R1")
+child.expect("#")
+child.sendline("end")
+child..expect("#")
